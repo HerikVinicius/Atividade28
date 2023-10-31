ab8aa836c5c36af4a60914c8a344755491d1b676
HerikVinicius <herikvinicius720@gmail.com>
2023-10-31 16:35:04 -0300

    first commit

---
+++ atvherik28.py
@@ -0,0 +1,29 @@
+# Exercício Python 28 : Crie um programa que leia o ano de nascimento de sete pessoas. No final,
+# mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+from datetime import date
+
+today = date.today()
+anoAtual = today.year
+from datetime import date 
+
+today = date.today() 
+anoAtual = today.year 
+
+idadeMaior = []
+IdadeMenor = []
+
+idadeMaior = [] 
+IdadeMenor = []
+
+for i in range(8):
+    anoNascimento = int(input("ano de nascimento"))
+  
+    if anoAtual - anoNascimento >= 18:
+
+        idadeMaior.append(anoNascimento)
+    else:
+        IdadeMenor.append(anoNascimento)
+
+
+print(idadeMaior)
+print(IdadeMenor)
